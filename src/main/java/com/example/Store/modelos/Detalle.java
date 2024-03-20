088b3efbcc41b93e69669ef0308ac15dc5e2c2bc
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2024-03-20 12:15:37 -0500

    cambios

--- src/main/java/com/example/Store/modelos/Detalle.java
+++ src/main/java/com/example/Store/modelos/Detalle.java
@@ -8,8 +8,20 @@ public class Detalle {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(name= "costoTotal", nullable  = false, length = 50)
+
     private Integer costoTotal;
+    @Column(name= "cantidadProducto", nullable  = false, length = 100)
     private Integer cantidadProductos;
 
 
+
+    public Detalle() {
+    }
+
+    public Detalle(Integer id, Integer costoTotal, Integer cantidadProductos) {
+        this.id = id;
+        this.costoTotal = costoTotal;
+        this.cantidadProductos = cantidadProductos;
+    }
 }
